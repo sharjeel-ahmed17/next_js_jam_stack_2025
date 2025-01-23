83af06429e138a691e3a46e09c067c3fccc1db79
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-01-23 18:06:44 +0500

    sanity migration

---
+++ index.js
@@ -0,0 +1,107 @@
+// (OP - DP / OP) * 100 = ANS%
+// title 
+// price
+// des
+// cat
+// dis
+// tags
+// image 
+// thumb
+[
+    {
+        "title": "Bold Nest",
+        "price": 260,
+        "tags": [
+          "bold ",
+          "nest ",
+          "furniture ",
+          "modern ",
+          "contemporary"
+        ],
+      "category": 
+            {
+                "id": 2,
+                "categoryTitle": "Living"
+            },
+       
+            "description": "Welcome to BoldNest—where fearless design meets comfort and creativity. Crafted for those who embrace individuality and bold expressions, BoldNest is more than just a piece of furniture; it’s a statement. With its striking design, exceptional comfort, and modern aesthetics, BoldNest is perfect for anyone looking to add a unique touch to their home or office.\n\nThe BoldNest collection combines daring colors, dynamic shapes, and high-quality materials to create an environment where style and comfort coexist in perfect harmony. Whether it’s a chair, sofa, or accent piece, each item is designed to stand out while offering a welcoming space for relaxation and enjoyment.\n\nIts sturdy construction and thoughtful design ensure that BoldNest not only makes a bold visual impact but also provides lasting durability. Perfect for those who are looking to break away from the ordinary and make their home a reflection of their bold personality, BoldNest is an ideal choice for creating an energetic and unique atmosphere.\n\nKey Features:\n\nBold and unique design that adds personality to any space\nHigh-quality materials for comfort, durability, and style\nAvailable in a variety of striking colors and patterns\nPerfect for modern homes and offices that embrace creativity\nVersatile design that complements contemporary and eclectic decor\nTransform your home into a space that reflects your bold, unique style with BoldNest—where standout design and comfort meet.",
+        "colors": [
+            "Blue",
+            "Red",
+            "Black"
+           
+          ],
+          "sizes": [
+            "S",
+            "XXL",
+            "XL",
+            "L"
+          ],
+        "images": [
+            "https://cdn.sanity.io/images/7xt4qcah/production/0654318eea4b8aea9234762af914d512e60ddc15-4160x4160.jpg",
+            "https://cdn.sanity.io/images/7xt4qcah/production/0654318eea4b8aea9234762af914d512e60ddc15-4160x4160.jpg",
+            "https://cdn.sanity.io/images/7xt4qcah/production/0654318eea4b8aea9234762af914d512e60ddc15-4160x4160.jpg",
+            "https://cdn.sanity.io/images/7xt4qcah/production/0654318eea4b8aea9234762af914d512e60ddc15-4160x4160.jpg"
+        ],
+        "thumbnail": "https://cdn.sanity.io/images/7xt4qcah/production/0654318eea4b8aea9234762af914d512e60ddc15-4160x4160.jpg",
+        
+        "discountPrice": 200,
+        "rating": "4.5",
+        "stock": 12,
+       
+        "minimunOrderQuantity": 10,
+        "sku": "LIV-RVD-001",
+        "reviews": [
+            {
+                "rating": 5,
+                "comments": "Perfect addition to our dining table decor!",
+                "date": "2025-01-15",
+                "reviwerName": "Alice Brown",
+                "reviwerEmail": "alice.brown@example.com"
+            }
+        ],
+        
+        
+        "general": {
+            "salesPackage": "1 Rustic Vase Set",
+            "modelNo": "RVD-123",
+            "brand": "Vintage Elegance",
+            "secondaryMaterial": ["Ceramic"],
+            "upholsteryMaterial": ["Cotton Fabric"], 
+            "upholsteryColors": ["Beige"], 
+            "configuration": "Single Piece"
+        },
+        "product": {
+            "fillingMaterial": "Foam",
+            "finishTypeColors": ["Rustic Beige", "Glossy White"],
+            "adjustableHeadrest": false,
+            "maximumLoadCapacity": "50 kg",
+            "originOfManufature": "India"
+        },
+        "dimensions": {
+            "width": "15 cm",
+            "height": "30 cm",
+            "depth": "15 cm",
+            "weight": "1.5 kg",
+            "seatHeight": "N/A",
+            "legHeight": "N/A"
+        },
+        "warrenty": {
+            "warrentyInformation": "1 year manufacturing warranty",
+            "warrentyServiceType": "support@vintageelegance.com",
+            "coveredInWarranty": "Manufacturing defects",
+            "NotCoveredWarranty": "Physical damage, mishandling, or water exposure",
+            "domesticWarranty": "3 months"
+        }
+    }
+]
+
+
+
+// cate
+
+
+
+
+
+
